4103f13f6d2ed4265cb65f5af28016845d3502fc
Arun Rawlani <arun.rawlani@mail.mcgill.ca>
2015-07-12 06:32:19 -0700

    Fix

--- Algorhythm/MusicLibrary.swift
+++ Algorhythm/MusicLibrary.swift
@@ -12,51 +12,51 @@ struct MusicLibrary {
     let library = [
         [
             "title": "DJ Mustard",
-            "description": "Get your morning going by singing along to these classic tracks as you hit the shower bright and early!",
+            "description": "Rap/Hip Hop",
             "icon": "dj_mustard.pdf",
             "largeIcon": "dj_mustard_large.pdf",
-            "backgroundColor": ["red": 255, "green": 204, "blue": 51, "alpha": 1.0],
+            "backgroundColor": ["red": 0, "green": 0, "blue": 0, "alpha": 1.0],
             "artists": ["Rack City", "Paranoid", "Show Me"]
         ],
         [
             "title": "Mumford & Sons",
-            "description": "For a band who had gigged pretty much nonstop since forming, Mumford & Sons’ five-month hiatus – which began when they completed their world tour for the Babel album in September 2013. ",
-            "icon": "running.pdf",
-            "largeIcon": "running_large.pdf",
+            "description": "Alternative",
+            "icon": "mumford_and_sons_small",
+            "largeIcon": "mumford_and_sons_large",
             "backgroundColor": ["red": 255, "green": 102, "blue": 51, "alpha": 1.0],
-            "artists": ["Avicii", "Calvin Harris", "Pitbull", "Iggy Azalea", "Rita Ora", "Drake", "Lil Wayne"]
+            "artists": ["I Will Wait", "Broken Crown", "Babel"]
         ],
         [
-            "title": "Joy Ride",
-            "description": "Let this eclectic playlist take you wherever your heart desires. Cruise along in style to these energetic beats.",
-            "icon": "helmet.pdf",
-            "largeIcon": "helmet_large.pdf",
+            "title": "Kendrick Lamar",
+            "description": "Rap/Hip Hop",
+            "icon": "kendrick_lamar_small",
+            "largeIcon": "kendrick_lamar_large",
             "backgroundColor": ["red": 153, "green": 102, "blue": 204, "alpha": 1.0],
-            "artists": ["Afrojack", "Kid Cudi", "Daft Punk", "Icona Pop", "Gesaffelstien", "Roksnoix", "deadmau5"]
+            "artists": ["King Kunta", "Money Trees", "Backseat Freestyle"]
         ],
         [
-            "title": "In The Zone",
+            "title": "Sam Smith",
             "description": "Keep calm and focus. Shut out the noise around you and grind away with some mind sharpening instrumental tunes.",
-            "icon": "laptop.pdf",
-            "largeIcon": "laptop_large.pdf",
+            "icon": "sam_smith_small",
+            "largeIcon": "sam_smith_large",
             "backgroundColor": ["red": 51, "green": 153, "blue": 204, "alpha": 1.0],
-            "artists": ["Dr. Dre", "Snoop Dogg", "Com Truise", "D12", "Flying Lotus", "Kanye West", "Rundfunk"]
+            "artists": ["Stay With Me", "Restart", "Together"]
         ],
         [
-            "title": "Button Masher",
+            "title": "Elton John",
             "description": "Turn up the speakers and get out of my way! The ultimate gaming playlist to get you hyped up and ready for the crazy fun that’s about to happen.",
-            "icon": "joystick.pdf",
-            "largeIcon": "joystick_large.pdf",
+            "icon": "elton_john_small",
+            "largeIcon": "elton_john_large",
             "backgroundColor": ["red": 51, "green": 204, "blue": 102, "alpha": 1.0],
-            "artists": ["Skrillex", "The Game", "2 Chainz", "Jay Z", "T.I.", "Rihanna", "Eminem"]
+            "artists": ["Rocket Man", "Rocket Man" ,  "Can You Feel The Love Tonight"]
         ],
         [
-            "title": "Futbal Remix",
+            "title": "Black Keys",
             "description": "There’s nothing like the world’s game. Kick around the field with this eclectic playlist from around the world. Futbal for life.",
-            "icon": "ball.pdf",
-            "largeIcon": "ball_large.pdf",
+            "icon": "black_keys_small",
+            "largeIcon": "black_keys_large",
             "backgroundColor": ["red": 255, "green": 102, "blue": 153, "alpha": 1.0],
-            "artists": ["Shakira", "Santana", "Wyclef Jean", "Aloe Blacc", "Pitbull", "Enrique Iglesias", "Ricky Martin"]
+            "artists": ["Lonely Boy", "Gold on the Ceiling", "Fever"]
         ]
     ]
 }
